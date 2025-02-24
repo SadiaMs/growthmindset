c65df28e591e2568f31617cbfbcf4c7fb0c74672
sadia-siddique <162547312+SadiaMs@users.noreply.github.com>
2025-02-24 14:38:36 +0500

    Create app.py
    this is my growthmind set islamic question

---
+++ app.py
@@ -0,0 +1,60 @@
+Islamic Studies
+Which of the following companion of Hazrat Muhammad(PBUH) was not among “Ashrah Mubashrah”?
+A. Hazrat Khalid bin Waleed (RA)
+B. Hazrat Saad bin abi waqas(RA)
+C. Hazrat Zubair Ibn e Awam (RA)
+D. Hazrat Talha(RA)
+
+In Islamic Law of Inheritance each female get half share of male standing at the same degree of relationship, except ________ who gets equal share as____.
+A. Sister_Brother
+B. Daugher_Son
+C. Mather_Father
+D. None of these (Female always get a half share as men)
+
+Umer Bin Abdel Aziz was 11 Caliph of ________.
+A. The Umayyed Caliphate
+B. The Abbasid Caliphate
+C. The Rashidun Caliphate
+D. Ottoman Ampire
+
+Which is the first mosque in Islamic History?
+A. Masjid e Nabvi
+B. Masjid e Quba
+C. Masjid Ul Haram
+D. Masjid e Aqsa
+
+Which wife of the Holy Prophet (SAWW) was alive at the time of the Karbala incident?
+A. Hazrat Umme Salma
+B. Hazrat Aiysha
+C. Hazrat Zainab
+D. All of the Above
+
+Which Caliph of Rashidun Caliph had the longest reign?
+A. Hazrat Abu Bakr (R.A)
+B. Hazrat Umer (R.A)
+C. Hazrat Uthman (R.A)
+D. Hazrat Ali (R.A)
+
+What is the maximum RAKATs in Chasht Prayer?
+A. 4
+B. 6
+C. 8
+D. 2
+
+Holy Quran has ____ Rukus?
+A. 540
+B. 552
+C. 554
+D. 558
+
+When was fasting made obligatory in Islam?
+A. 2 Hijri
+B. 3 Hijri
+C. 4 Hijri
+D. 5 Hijri
+
+In which holy battle enemy of the Holy Prophet Abu Jahl was killed?
+A. Gazwa Badr
+B. Gazwa Ohad
+C. Gazwa Khyber
+D. Gazwa Tabook
